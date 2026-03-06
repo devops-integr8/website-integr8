1d9ccc60576502d814dcace5b1471324ef5064f6
devops-integr8 <devops@integr8.com.ph>
2026-03-06 09:42:30 +0800

    Add Mission Vision

---
+++ app/about/MissionVision.tsx
@@ -0,0 +1,21 @@
+import HoverCard from "@/components/HoverCard";
+
+export default function MissionVision() {
+  return (
+    <section className="relative bg-white py-16 px-16">
+      <div className="grid md:grid-cols-2 gap-12  max-h-3xl  mx-auto">
+        <HoverCard
+          title="MISSION"
+          description="To empower businesses by delivering software solutions that enhance productivity, efficiency, resulting to success."
+          background="Mission.png"
+        />
+
+        <HoverCard
+          title="VISION"
+          description="Optimizing businesses with one powerful software solution."
+          background="Vision.png"
+        />
+      </div>
+    </section>
+  );
+}
