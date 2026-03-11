f710e8f7b8e317b219834afaf4a281691d1acd17
devops-integr8 <devops@integr8.com.ph>
2026-03-11 08:58:07 +0800

    revised the hovercards for mission vision

--- components/HoverCard.tsx
+++ components/HoverCard.tsx
@@ -6,17 +6,24 @@ type HoverCardProps = {
 
 const HoverCard = ({ title, description, background }: HoverCardProps) => {
   return (
-    <div className="group relative w-xl h-100  max-w-full overflow-hidden cursor-pointer">
-      {/* Background */}
+    <div className="group relative w-xl h-100 max-w-full overflow-hidden cursor-pointer">
+      {/* Gradient */}
+      <div className="absolute inset-0 bg-gradient-to-b from-[#0b0f17] via-[#1b3566] to-[#224d9a]" />
+
+      {/* Vignette */}
+      <div className="absolute inset-0 bg-black/30" />
+      {/* Watermark Icon */}
       <div
-        className=" w-[570px] h-[570px] absolute bg-contain  bg-no-repeat  transition-transform group-hover:scale-1 duration-[1000ms] -translate-x-20  -translate-y-15"
+        className="absolute w-[570px] h-[570px] bg-contain bg-no-repeat
+    opacity-[0.50]
+    transition-all duration-[1200ms] will-change-transform
+    group-hover:scale-[1.6] group-hover:opacity-0
+    -translate-x-20 -translate-y-16"
         style={{ backgroundImage: `url(${background})` }}
       />
 
-      {/* Gradient Overlay */}
-      <div className="absolute inset-0 bg-linear-to-b from-[#224d9b] to-black/90 transition-all duration-500 group-hover:from-[#224d9b] group-hover:to-black/90" />
-
-      <div className="absolute top-1/3 space-y-4 x-40 ">
+      {/* Content */}
+      <div className="absolute top-1/3 space-y-4 px-16">
         {/* Title */}
         <div className="w-full flex justify-center transition-all duration-500 group-hover:items-start">
           <h2
@@ -33,7 +40,7 @@ const HoverCard = ({ title, description, background }: HoverCardProps) => {
         {/* Description */}
         <div
           className="
-          w-full font-normal flex px-10 
+          w-full font-normal px-10 
           opacity-0 translate-y-16
           transition-all duration-[1500ms] ease-[cubic-bezier(0.22,1,0.36,1)]
           group-hover:opacity-100 group-hover:translate-y-0"
