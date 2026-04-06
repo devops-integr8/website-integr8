857efc0c4e8a6e84556a40bd1d1c154067ba54a7
Jayyy-source <luiscriverajr@gmail.com>
2026-04-06 09:22:45 +0800

    removed section and fix responsiveness

--- app/about/MissionVision.tsx
+++ app/about/MissionVision.tsx
@@ -2,8 +2,8 @@ import HoverCard from "@/components/HoverCard";
 
 export default function MissionVision() {
   return (
-    <section className="relative bg-white py-16 px-16">
-      <div className="grid md:grid-cols-2 gap-12  max-h-3xl  mx-auto">
+    <div className="w-full">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 sm:gap-8 lg:gap-12 max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
         <HoverCard
           title="MISSION"
           description="To empower businesses by delivering software solutions that enhance productivity, efficiency, resulting to success."
@@ -16,6 +16,6 @@ export default function MissionVision() {
           background="visionIcon.png"
         />
       </div>
-    </section>
+    </div>
   );
 }
