dd509f31b788f1f412dc3fbc8fa8ca1c563856fa
Jayyy-source <luiscriverajr@gmail.com>
2026-05-13 10:29:09 +0800

    modified heading

--- app/products/productsOnyx/onyxIntro.tsx
+++ app/products/productsOnyx/onyxIntro.tsx
@@ -3,9 +3,9 @@ import IntroCards from "./introCards";
 const OnyxIntro = () => {
   return (
     <div className="min-h-screen bg-white flex items-center justify-center px-6 py-16">
-      <div className="max-w-3xl w-full">
+      <div className="max-w-6xl w-full">
         <div className="text-center mb-10 pt-5">
-          <h1 className="text-4xl font-bold text-[#02208C]  mb-3">
+          <h1 className="text-[42px] font-bold text-[#0818a8]  mb-3">
             Why Choose ONYX Point-of-Sale System?
           </h1>
           <p className="text-black text-md leading-relaxed max-w-xl mx-auto">
